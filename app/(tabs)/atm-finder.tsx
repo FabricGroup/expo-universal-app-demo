c7456757bd364a3fce02ea597d63e054cd719a2a
Anish George <anish.george@fabricgroup.com.au>
2024-09-28 15:07:43 +1000

    feat: Mobile ATM finder (#2)
    * Start working on atm finder feature
    
    * Complete a barebones atm finder implementation

--- app/(tabs)/atm-finder.tsx
+++ app/(tabs)/atm-finder.tsx
@@ -1,12 +1,14 @@
-import { Paragraph, YStack } from "tamagui";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Paragraph, YStack } from "tamagui";
+import { AtmFinder } from "../../components/features/AtmFinder";
 
 export default function AtmFinderScreen() {
   const insets = useSafeAreaInsets();
-  /** Render Expo map */
   return (
-    <YStack pt="$3" px="$10">
+    <YStack pt="$3" px="$3" f={1}>
       <Paragraph>This page will allow you to find the nearest ATM</Paragraph>
+      {/* The atm finder depends on the platform */}
+      <AtmFinder />
     </YStack>
   );
 }
