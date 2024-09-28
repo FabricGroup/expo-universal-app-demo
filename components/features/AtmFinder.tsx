5dbe08f83badd1d493d594aee3efaa270d73a7c6
Anish George <anish.george@fabricgroup.com.au>
2024-09-28 14:42:00 +1000

    Start working on atm finder feature

---
+++ components/features/AtmFinder.tsx
@@ -0,0 +1,7 @@
+import { H3 } from "tamagui";
+
+export function AtmFinder() {
+  return (
+    <H3>this is a mobile atm finder</H3>
+  )
+}
---
+++ components/features/AtmFinder.web.tsx
@@ -0,0 +1,7 @@
+import { H3 } from "tamagui";
+
+export function AtmFinder() {
+  return (
+    <H3>this is a web based atm finder</H3>
+  )
+}
