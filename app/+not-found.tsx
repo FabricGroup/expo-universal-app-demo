d4289cf3c4caf84f4f2d253df7fdc9acd3a3c488
Anish George <anish.george@fabricgroup.com.au>
2024-10-09 19:54:28 +1100

    Feat/cleanup (#5)
    * Some UI changes
    
    * Some cleanup

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,18 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { H3, Paragraph } from 'tamagui';
+import { StandardScreen } from '../components/StandardScreen';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+      <StandardScreen>
+        <H3>This screen doesn't exist.</H3>
+        <Link href="/">
+          <Paragraph>Go to home screen!</Paragraph>
         </Link>
-      </ThemedView>
+      </StandardScreen>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
